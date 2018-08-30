ea0950b4f5a25efc17d5653f51dd8d294f050e1a
ponraj <jarnop92@gmail.com>
2018-08-30 14:25:37 +0530

    sjakhj

---
+++ app/src/main/java/models/Common.java
@@ -0,0 +1,24 @@
+package models;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Common {
+    public static Date convertDate(String inputString, String inputStringFormat) {
+        SimpleDateFormat format = new SimpleDateFormat(inputStringFormat);
+        try {
+            Date date = format.parse(inputString);
+            return date;
+        } catch (ParseException pr) {
+            return null;
+        }
+
+    }
+
+    public static String convertDateString(Date date, String dateFormat) {
+        SimpleDateFormat format = new SimpleDateFormat(dateFormat);
+        String str = format.format(date);
+        return str;
+    }
+}
