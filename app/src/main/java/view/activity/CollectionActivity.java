2228a3d74cc0338893b8711e10afbb7fd37d2cd1
sakthivel <pkksshakthi@gmail.com>
2018-09-27 14:12:53 +0530

    added new menu

--- app/src/main/java/view/activity/CollectionActivity.java
+++ app/src/main/java/view/activity/CollectionActivity.java
@@ -46,7 +46,7 @@ public class CollectionActivity extends AppCompatActivity {
     ArrayList<Uri> imagesUriList;
     ArrayList<String> encodedImageList;
     String imageURI;
-    public static final String urlUpload = "http://sachinverma.co.in/services/upload_multiple_images/upload_multiple_images.php";
+    public static final String urlUpload = "/bigflowdemo/FileUpload";
     public static final int REQCODE = 100;
     public static final String imageList = "imageList";
     public static final String imageName = "name";
